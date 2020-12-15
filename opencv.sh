7f14f4470431567014371104747808172974ca54
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2020-12-15 10:17:32 -0300

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -9,5 +9,6 @@ wget -O opencv.zip https://github.com/opencv/opencv/archive/master.zip
 unzip opencv.zip
 mv opencv-master opencv
 mkdir -p build && cd build
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 cmake -S /home/pi/opencv
 echo "FIM"
