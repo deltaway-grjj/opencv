36f5a67b7c0b4eb6d5b6dd5d1b5b7fbd3aa096a3
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-01-15 13:48:06 -0300

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -7,7 +7,7 @@ git \
 make \
 ffmpeg
 wget -O ~/opencv.zip https://github.com/opencv/opencv/archive/master.zip
-unzip opencv.zip
+unzip ~/opencv.zip
 mv ~/opencv-master ~/opencv
 mkdir -p ~/opencv/build && cd ~/opencv/build
 #export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
