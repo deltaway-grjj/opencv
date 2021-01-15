248f04b2a2fc0ccff15e7b8b8e1270e3332214cc
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-01-15 13:58:46 -0300

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -6,9 +6,9 @@ g++ \
 git \
 make \
 ffmpeg
-wget -O ~/opencv.zip https://github.com/opencv/opencv/archive/master.zip
-unzip ~/opencv.zip
-mv ~/opencv-master ~/opencv
+wget -O ~/opencv/opencv.zip https://github.com/opencv/opencv/archive/master.zip
+unzip ~/opencv/opencv.zip
+mv ~/opencv/opencv-master ~/opencv/opencv
 mkdir -p ~/opencv/build && cd ~/opencv/build
 #export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-armhf"
@@ -16,7 +16,7 @@ export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-armhf"
 sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
-cmake -S ~/opencv -B ~/opencv/build/
+cmake -S ~/opencv/opencv -B ~/opencv/build/
 sudo sed -i 's/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
