89a39af29749bd263684c0c18fcc8dd38161431f
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-05-06 14:28:59 -0300

    Update opencv_offline.sh

--- opencv_offline.sh
+++ opencv_offline.sh
@@ -1,1 +1,24 @@
 
+#!/bin/sh
+sudo apt update && sudo apt -y install \
+cmake \
+ffmpeg \
+g++ \
+git \
+make \
+#openjdk-8-jdk
+wget -O ~/opencv/opencv.zip https://github.com/opencv/opencv/archive/master.zip
+unzip ~/opencv/opencv.zip
+mv ~/opencv/opencv-master ~/opencv/opencv
+mkdir -p ~/opencv/build && cd ~/opencv/build
+#export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-armhf"
+#https://www.learnopencv.com/build-and-install-opencv-4-for-raspberry-pi/
+sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile stop
+sudo /etc/init.d/dphys-swapfile start
+cmake -B ~/opencv/build/ -D ANT_EXECUTABLE=/usr/bin/ant -D BUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_python=OFF -DCMAKE_BUILD_TYPE=Release -S ~/opencv/opencv
+make -j4
+sudo sed -i 's/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile stop
+sudo /etc/init.d/dphys-swapfile start
