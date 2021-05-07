342ec5f0cd1e99dceb1a0232faf1a1da7130d76c
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-05-07 11:04:25 -0300

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -1,12 +1,70 @@
 
 #!/bin/sh
-sudo apt update && sudo apt -y install \
+sudo apt update && sudo apt -y full-upgrade && sudo apt -y install \
+ant \
+autoconf \
+autoconf-archive \
+automake \
+build-essential \
+bzip2 \
+ccache \
+clang \
 cmake \
+default-jdk \
+file \
 ffmpeg \
-g++ \
+freeglut3-dev \
+gfortran \
 git \
+git-core \
+gzip \
+g++ \
+libasound2-dev \
+libass-dev \
+libatlas-base-dev \
+libavcodec-dev \
+libavformat-dev \
+libcanberra-gtk* \
+libfreetype6-dev \
+libgnutls28-dev \
+libgtk-3-dev \
+libgtk2.0-dev \
+libjpeg-dev \
+libpng-dev \
+libprotobuf-dev \
+libsdl2-dev \
+libswscale-dev \
+libtiff-dev \
+libtool \
+libusb-dev \
+libv4l-dev \
+libva-dev \
+libvdpau-dev \
+libvorbis-dev \
+libx264-dev \
+libxcb1-dev \
+libxcb-shm0-dev \
+libxcb-xfixes0-dev \
+libxvidcore-dev \
 make \
-#openjdk-8-jdk
+maven \
+nasm \
+openjdk-8-jdk \
+patch \
+perl \
+pkg-config \
+protobuf-compiler \
+python \
+swig \
+tar \
+texinfo \
+unzip \
+wiringpi \
+wget \
+xmlstarlet \
+yasm \
+zlib1g \
+zlib1g-dev
 wget -O ~/opencv/opencv.zip https://github.com/opencv/opencv/archive/master.zip
 unzip ~/opencv/opencv.zip
 mv ~/opencv/opencv-master ~/opencv/opencv
