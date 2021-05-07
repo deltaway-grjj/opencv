bd64a48767665c7cdf12699ce4180f3322cd4cdc
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-05-07 16:33:47 -0300

    Update opencv_offline.sh

--- opencv_offline.sh
+++ opencv_offline.sh
@@ -3,24 +3,10 @@
 wget ftp://teste:@192.168.10.238/packages.tar.gz
 tar -xzvf packages.tar.gz
 sudo dpkg -i archives/*.deb
-#wget -O ~/opencv/opencv.zip https://github.com/opencv/opencv/archive/master.zip
-#unzip ~/opencv/opencv.zip
-#unzip opencv/opencv.zip -d opencv/
 wget ftp://teste:@192.168.10.238/opencv.tar.gz
 tar -xzvf opencv.tar.gz
-#mv ~/opencv/opencv-master ~/opencv/opencv
-#mkdir -p ~/opencv/build && cd ~/opencv/build
-#mkdir opencv/opencv-master/build
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
-#export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-armhf"
-#https://www.learnopencv.com/build-and-install-opencv-4-for-raspberry-pi/
-#sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
-#sudo /etc/init.d/dphys-swapfile stop
-#sudo /etc/init.d/dphys-swapfile start
-#cmake -B ~/opencv/build/ -D ANT_EXECUTABLE=/usr/bin/ant -D BUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_python=OFF -DCMAKE_BUILD_TYPE=Release -S ~/opencv/opencv
 cmake -B opencv/build/ -D ANT_EXECUTABLE=/usr/bin/ant -D BUILD_PERF_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DBUILD_opencv_python=OFF -DCMAKE_BUILD_TYPE=Release -S opencv
-#make -j4
 make -j4 -C opencv/build/
-#sudo sed -i 's/CONF_SWAPSIZE=2048/CONF_SWAPSIZE=100/g' /etc/dphys-swapfile
-#sudo /etc/init.d/dphys-swapfile stop
-#sudo /etc/init.d/dphys-swapfile start
+ls opencv/build/bin/
+ls opencv/build/lib/
